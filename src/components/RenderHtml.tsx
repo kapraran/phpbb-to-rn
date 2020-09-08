2c5800959e1299a6418cab5cbf1ecf2ec4bdd0a5
Nikos Kapraras <iamnikosk93@gmail.com>
2020-09-09 01:17:42 +0300

    added quote rendering

--- src/components/RenderHtml.tsx
+++ src/components/RenderHtml.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { View, StyleSheet, Image } from 'react-native';
-import { Text } from 'react-native-paper';
+import { Text, Colors, Caption, Paragraph } from 'react-native-paper';
 import { parseHTML } from '../utils/utils';
 import NetworkImage from './NetworkImage';
 
@@ -8,30 +8,56 @@ interface Props {
   html: string;
 }
 
-const renderElement = (node: ChildNode, index: number) => {
-  switch (node.nodeName) {
+const renderQuote = (quote: HTMLElement, index: number, prefixKey: string) => {
+  const username = quote.children[0].innerHTML.split(' ')[0];
+
+  return (
+    <View style={styles.quote} key={prefixKey + index}>
+      <Caption style={styles.quoteHeader}>{username}:</Caption>
+      <View style={styles.quoteContent}>
+        {renderNodes(
+          Array.from(quote.children[1].childNodes),
+          prefixKey + index + 'q',
+        )}
+      </View>
+    </View>
+  );
+};
+
+const renderElement = (node: ChildNode, index: number, prefixKey: string) => {
+  const element = node as HTMLElement;
+
+  switch (element.tagName) {
+    case 'DIV':
+      if (element.className == 'quotewrapper') {
+        return renderQuote(element, index, prefixKey);
+      }
     case 'BR':
-      return <View key={index} style={{ height: 8 }} />;
+      return <View key={prefixKey + index} style={{ height: 8 }} />;
     case 'IMG':
-      return (
-        <NetworkImage
-          key={index}
-          uri={(node as HTMLImageElement).src}></NetworkImage>
-      );
+    // return (
+    //   <NetworkImage
+    //     key={index}
+    //     uri={(node as HTMLImageElement).src}></NetworkImage>
+    // );
     default:
       return null;
   }
 };
 
-const renderNode = (node: ChildNode, index: number) => {
+const renderNode = (node: ChildNode, index: number, prefixKey: string) => {
   switch (node.nodeType) {
     case node.TEXT_NODE:
-      return <Text key={index}>{node.textContent}</Text>;
+      return <Paragraph key={prefixKey + index}>{node.textContent}</Paragraph>;
     case node.ELEMENT_NODE:
-      return renderElement(node, index);
+      return renderElement(node, index, prefixKey);
     default:
       return null;
   }
+};
+
+const renderNodes = (nodes: ChildNode[], prefixKey: string) => {
+  return nodes.map((node, index) => renderNode(node, index, prefixKey));
 };
 
 const RenderHtml = (props: Props) => {
@@ -52,7 +78,7 @@ const RenderHtml = (props: Props) => {
 
   return (
     <View style={styles.container}>
-      <View>{nodes.map((node, index) => renderNode(node, index))}</View>
+      <View>{renderNodes(nodes, ':')}</View>
     </View>
   );
 };
@@ -60,6 +86,21 @@ const RenderHtml = (props: Props) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    padding: 16,
+  },
+  quote: {
+    borderWidth: 1,
+    borderColor: Colors.grey300,
+    borderRadius: 4,
+    backgroundColor: Colors.grey100,
+  },
+  quoteHeader: {
+    paddingHorizontal: 16,
+    paddingVertical: 8,
+    borderBottomColor: Colors.grey200,
+    borderBottomWidth: 1,
+  },
+  quoteContent: {
     padding: 16,
   },
 });
